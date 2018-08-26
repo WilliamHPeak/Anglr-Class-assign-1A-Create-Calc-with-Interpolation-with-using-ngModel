ab08684f23f3d973e32c819d25d512fffba1c9ba
user <user@users-Macintosh.local>
2018-08-26 11:52:59 -0400

    Angular 2 Class Assign 1 using Interpolation with out ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,81 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myproject';
-}
+  date: Date;  // date object for piping
+
+  constructor() {
+    this.date = new Date();     /* create an instance of date, when the AppComponent object
+                                   is being instantiated.
+                                */
+  }
+
+  /* Note: a and be are operands in this calculator program
+       - iniitialized to not particular values
+       - the operands, when being used in divison are as follows:
+           a - will always be the dividen
+           b - will always the the division
+  */
+  a = 500;
+  b = 5;
+  result = 0;
+  operator = '+';
+  ErrorMsg = '';
+
+  resetOperator()
+  {
+    //document.getElementById('operatorNg').innerText = "";
+    this.operator="";
+  }
+  resetDividend()
+  {
+    //document.getElementById('operatorNg').innerText = "";
+    this.ErrorMsg="";
+  }
+  calculateUpdate(newOperator,newA,newB) {
+
+
+     var NanA = +newA;  /* using the Unary + to convert the incoming strings to numbers*/
+     var NanB = +newB;
+     if((NanA != NanA )|| (NanB != NanB)) /* Nan can only be compared to Nam */
+      {
+        document.getElementById('divideByZero').className="CenterError";
+        this.ErrorMsg = 'You must Enter two valid Numbers';
+        return;
+      }
+     var maybeANumberA = +newA;
+     var maybeANumberB = +newB;
+
+    document.getElementById('operatorNg').className="Center";
+
+        this.a = parseFloat(newA.replace(/,/g, ''));newA;
+        this.b = parseFloat(newB.replace(/,/g, ''));newB;
+
+        switch (newOperator) {
+          case '+':
+            this.result = this.a + this.b;
+            break;
+          case '-':
+            this.result = this.a - this.b;
+            break;
+          case '*':
+            this.result = this.a * this.b;
+            break;
+          case '/':
+            if(this.b <= 0)
+            {
+              document.getElementById('divideByZero').className="CenterError";
+              this.ErrorMsg = 'Dividend must be > 0 ';
+
+              break;
+            }
+            else{
+              this.result = this.a / this.b;
+              break;
+            }
+          default:
+            this.operator="Sorry, only +,-,*,/ operators allowed";
+            document.getElementById('operatorNg').className="CenterError";
+
+        }
+    }
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,65 @@
+
+
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';// whp must add for two binding ngModel
 import { AppComponent } from './app.component';
 
+//import { StrDirectiveComponent } from './str-directive/str-directive.component';
+//import { BrowserModule } from '@angular/platform-browser';
+//import { NgModule } from '@angular/core';
+//import { FormsModule} from '@angular/forms';
+//import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule,CommonModule ], //< added FormsModule here
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ],
+
+// imports: [
+//   BrowserModule,
+//   FormsModule
+// ],
+//  providers: []
+// bootstrap: [AppComponent]
+
+//@NgModule({
+//  declarations: [
+//  [BrowserModule, AppComponent],
+//  FormsModule
+//     StrDirectiveComponent
+//  ],
+//  imports: [
+//    BrowserModule,
+//    FormsModule              //whp must add for two way binding ngModel
+//  ],
+//  providers: [],
+// bootstrap: [AppComponent]
+ //bootstrap: [StrDirectiveComponent]
 })
 export class AppModule { }
+
+
+
+
+
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
